410d8f488b803e99f367daa9c9f829f1e94d2031
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-22 13:00:30 +0700

    Menambahkan fungsi DynamicSelectedTextField

---
+++ app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/widget/DynamicSelectedView.kt
@@ -0,0 +1,26 @@
+package com.example.pam_nvvm2_andini_copy.ui.widget
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DynamicSelectedTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+)
