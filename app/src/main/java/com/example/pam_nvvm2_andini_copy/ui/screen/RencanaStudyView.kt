5eb9657306bc39f7f2a4a4a0a3f6343ffee9a7a3
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-22 13:16:55 +0700

    Menambahkan tampilan header dengan logo berbentuk, nama dan NIM mahasiswa yang ditampilkan di sebelah kiri, ikon notifikasi di sebelah kanan.

--- app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/screen/RencanaStudyView.kt
@@ -58,5 +58,45 @@ fun RencanaStudyView(
     }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
 
-
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ) {
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logoumy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column (modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
+            Box {
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
+        }
+    }
 }
