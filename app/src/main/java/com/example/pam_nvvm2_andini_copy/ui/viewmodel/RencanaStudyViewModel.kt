d70a9e81ba5d233270ae7405ff9663c1db6d0b4a
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-22 12:58:30 +0700

    Menambahkan RencanaStudyViewModel dengan pengelolaan state dan fungsi pembaruan data untuk penanganan KRS.

---
+++ app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.pam_nvvm2_andini_copy.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pam_nvvm2_andini_copy.model.RencanaStudy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel : ViewModel(){
+    private val _krsState = MutableStateFlow(RencanaStudy())
+    val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String) {
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            mataKuliah = ls[0],
+            kelas = ls[1]
+        )
+        }
+    }
+}
