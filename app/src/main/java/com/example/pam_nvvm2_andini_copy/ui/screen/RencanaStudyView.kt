ab5bc74a630a44dfaabe41a35a6018fb5bab5379
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-22 13:21:05 +0700

    Menambahkan tombol "Kembali" dan "Lanjut" dimana tombol "Lanjut" hanya dapat diklik jika checkbox persetujuan telah dicentang.

--- app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/screen/RencanaStudyView.kt
@@ -168,7 +168,19 @@ fun RencanaStudyView(
                         fontWeight = FontWeight.Light, fontSize = 10.sp
                     )
                 }
-
+                Spacer(modifier = Modifier.padding(8.dp))
+                Row (
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Button (onClick = { onBackButtonClicked() } ) {
+                        Text(text = "Kembali")
+                    }
+                    Button (onClick = { onSubmitButtonClicked(listData) },
+                        enabled = checked) {
+                        Text(text = "Lanjut")
+                    }
+                }
             }
         }
     }
