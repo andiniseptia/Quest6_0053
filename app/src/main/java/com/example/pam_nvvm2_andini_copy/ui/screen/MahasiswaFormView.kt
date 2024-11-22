7f8a40478ce1eb20493c3ff5d49697b70ecc16c5
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-22 13:12:09 +0700

    Menambahkan kolom dengan teks instruksi untuk mengisi data

--- app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/screen/MahasiswaFormView.kt
+++ app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/screen/MahasiswaFormView.kt
@@ -102,6 +102,23 @@ fun MahasiswaFormView(
                     )
                 )
                 .fillMaxSize(),
-        )
+        ) {
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Masukkan Data Kamu",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 19.sp
+                )
+                Text(
+                    text = "isi sesuai data yang kamu daftarkan",
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
     }
 }
