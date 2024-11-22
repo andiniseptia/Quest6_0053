1d5452328b2bc6fc384e4c599c4658456d64b41a
andiniseptia <andiniseptiandri124@gmail.com>
2024-11-22 13:22:59 +0700

    Menambahkan fungsi TampilView dengan header yang menampilkan logo universitas dan teks "Data Mahasiswa"

---
+++ app/src/main/java/com/example/pam_nvvm2_andini_copy/ui/screen/TampilView.kt
@@ -0,0 +1,70 @@
+package com.example.pam_nvvm2_andini_copy.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pam_nvvm2_andini_copy.R
+import com.example.pam_nvvm2_andini_copy.model.Mahasiswa
+import com.example.pam_nvvm2_andini_copy.model.RencanaStudy
+
+
+@Composable
+fun TampilView(
+    mahasiswa: Mahasiswa,
+    rencanaStudy: RencanaStudy,
+    onBackToMainClicked: () -> Unit
+) {
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ), horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.logoumy
+                ),
+                contentDescription = "",
+                modifier = Modifier.size(45.dp)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Data Mahasiswa",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+
+    }
+}
+
